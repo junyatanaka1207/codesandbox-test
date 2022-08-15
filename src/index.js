0b3eac9c5f749671bf8b254f23a88921dcea6023
junyatanaka1207 <61687637+junyatanaka1207@users.noreply.github.com>
2022-08-15 14:49:52 +0700

    new

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
-import "./styles.css";
+/**
+ * const let等の変数宣言
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+//--------var-----------------
+// var val1 = "var変数";
+// console.log(val1);
+
+// //varは上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// //var変数は再宣言可能
+// var val1 = "val1を再宣言";
+// console.log(val1);
+
+//--------let-----------------
+// let val2 = "let変数";
+// console.log(val2);
+
+// //let変数は上書き可能
+// val2 = "val2上書き";
+// console.log(val2);
+
+// //letは再宣言できない
+// let val2 = "fafafasdf";
+
+//--------const-----------------
+// const val3 = "conat1変数";
+// console.log(val3);
+
+//conat変数は上書き不可能
+// val2 = "val2上書き";
+
+// ///letは再宣言できない
+// const val3 = "fafafasdf";
+
+//オブジェクト作成　基本constを使う
+// //オブジェクトの中身はconst変数でも変更できてしまう　追加や削除もできる
+// const val4 = {
+//   name: "junyua",
+//   age: 22,
+// };
+// val4.name = "tanaka"; //変更
+// val4.adress = "shizuoka"; //追加
+// console.log(val4);
