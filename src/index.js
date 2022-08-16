9ac81db5e708676891466e56db12088a3be6c5f9
junyatanaka1207 <61687637+junyatanaka1207@users.noreply.github.com>
2022-08-16 16:01:05 +0700

    new

--- src/index.js
+++ src/index.js
@@ -135,8 +135,91 @@
 // sumFunc(...arr1);
 
 //まとめる（展開の逆）
-const arr2 = [1, 2, 3, 4, 5];
-const [num1, num2, ...arr3] = arr2;
-console.log(num1);
-console.log(num2);
-console.log(arr3);
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2; //分割代入
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー　結合
+// const arr4 = [10,20];
+// const arr5 = [30,40];
+
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// const arr7 = [...arr4,...arr5];
+// console.log(arr7);
+
+//-----------------map flterを使った配列の処理-----------------
+//const nameArr = ["junya", "tanaka", "jake"];
+
+// for (let index = 0; index < nameArr.length; index++){
+//   console.log(nameArr[index]);
+// }
+
+// for (let index = 0; index < nameArr.length; index++){
+//   console.log(`${index + 1}番目は${nameArr[index]}です`);
+// }
+
+// const nameArr2 = nameArr.map((name) =>{
+//   return name;
+// })
+// console.log(nameArr2);
+
+//nameArr.map((name) => console.log(name));
+
+//nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// const numArr = [1,2,3,4,5];
+// const newNumArr = numArr.filter((num) =>{
+//   return num % 2 === 0;
+// })
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name)=>{
+//   if (name === "jake"){
+//     return name
+//   }else{
+//     return `${name}さん`
+//   }
+// })
+// console.log(newNameArr);
+
+//-----------------3項演算子-----------------
+// ある条件？trueの処理:falseの処理
+// const val1 = 1 < 0 ? "it's treu" :"it's false";
+// console.log(val1);
+
+// const num = 986598;
+// //console.log(num.toLocaleString());
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString():'数値を入力してください';
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "over 100" : "no problem";
+// };
+// console.log(checkSum(50, 40));
+
+//-----------------論理演算子の本当の意味を知る-----------------
+// const flag1 = true;
+// const flag2 = true;
+
+// if (flag1 || flag2){
+//   console.log("one or two is true");
+// }
+// if (flag1 && flag2){
+//   console.log("one and two is true");
+// }
+
+// ||は左側がfalseなら右を返す　trueならそのまま左を返す
+// const num = null ;
+// const fee = num || "no setteing";
+// console.log(fee);
+
+// &&は左側がtrueなら右を返す　trueならそのまま左を返す
+
+// const num2 = 100;
+// const fee2 = num2 && "setteings are ok";
+// console.log(fee2);
